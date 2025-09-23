4fa4deb3b19ac15db34c1881146113fc430b2936
Elina <elina@MacBook-Pro-Elina.local>
2025-09-23 22:41:44 +0300

    new

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,22 +5,20 @@
  */
 function makeInfinityAdder() {
   let sum = 0;
-    return function adder(x) { 
-        
-     if (x === undefined) {
-      const result = sum;
-      sum = 0;
-      return result;
-     } 
 
-     sum +=x;
-      return adder;
+  return function adder(x) {
+    if (x === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
     }
 
- return adder
+    sum += x;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
-
-
-  
